0a40fa3c9c0e1cf28fd4714bef8cf77c64fa55de
Matas Strimaitis <matasmatasp@gmail.com>
2021-02-13 10:02:03 +0200

    Serialize location class

--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace JsonDemo2
 {
@@ -10,6 +12,21 @@ namespace JsonDemo2
     {
         static void Main(string[] args)
         {
+            var location = new Location
+            {
+                X = 5,
+                Y = 6
+            };
+
+            var json = JsonConvert.SerializeObject(location);
+
+            File.WriteAllText($"{AppDomain.CurrentDomain.BaseDirectory}location.json",json);
         }
+    }
+
+    public class Location
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
     }
 }
